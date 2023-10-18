9cfa53bdd2ef391abaebad24fc2293efda006deb
enoch777 <yangenzou@gmail.com>
2023-10-18 11:52:07 +0800

    first

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+//import { Main } from "next/document";
+//import { mainModule } from "process";
+
+const About = () => {
+    return (
+        <main>
+            <p>
+                It is about me.
+            </p>
+        </main>
+    )
+   
+
+}
+
+export default About;
